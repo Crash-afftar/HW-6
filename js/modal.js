465087079c23eeb925744897d17cba21022650c3
Crash-afftar <sashakhymenko88@gmail.com>
2022-11-07 22:44:40 +0200

    HW-6

---
+++ js/modal.js
@@ -0,0 +1,35 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
+    // menuList: document.querySelector('.mob-menu-list'),
+  };
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  // refs.menuList.addEventListener('click', removeMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+  // function removeMenu() {
+  //   refs.menu.classList.add('is-hidden');
+  //   refs.body.classList.remove('no-scroll');
+  // }
+})();
